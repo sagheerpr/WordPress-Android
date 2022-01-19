2bab556123611e38e75ffec6a9c9f07d073f49d4
Antonis Lilis <antonis.lilis@automattic.com>
2022-01-19 19:19:26 +0200

    Update experiment version

--- WordPress/src/main/java/org/wordpress/android/util/experiments/LandOnTheEditorABExperiment.kt
+++ WordPress/src/main/java/org/wordpress/android/util/experiments/LandOnTheEditorABExperiment.kt
@@ -6,6 +6,6 @@ class LandOnTheEditorABExperiment
 @Inject constructor(
     exPlat: ExPlat
 ) : Experiment(
-        name = "wpandroid_land_in_the_editor_phase1_v2",
+        name = "wpandroid_land_in_the_editor_phase1_v3",
         exPlat
 )
